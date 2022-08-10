ebbd3994a7bfbe652b3b1af9c4d4725910eac027
Ayush-0801 <ayush19301@iiitd.ac.in>
2022-08-10 21:53:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -98,7 +98,8 @@
  //Play the song , finds the active spotify player if device id not specified
  app.post('/playback',async (req, res) => {
    res.setHeader('Content-Type', 'application/json');
-   const play= await spotifyApi.play({"device_id":req.body.player},{
+   const play= await spotifyApi.play({
+      "device_id":req.body.player,
       "uris": req.body.song,
       "position_ms":0,
    }).then(function() {
